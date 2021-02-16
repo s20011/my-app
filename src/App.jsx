d63a8c8e48c30edcce1de4afe1041e2022d06b6d
s20011 <s20011@std.it-college.ac.jp>
2021-02-16 10:48:07 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
+import { Button } from '@material-ui/core'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     //this.apikey = '42f670a29c788aa9'
     //this.URI1 = `https://webservice.recruit.co.jp/hotpepper/large_area/v1/?key=${this.apikey}&format=json`
-    this.URI = 'https://connpass.com/api/v1/event/?keyword=沖縄県'
-    this.state = { text: null }
+    this.URI = `https://connpass.com/api/v1/event/?keyword=${this.state.query}`
+    this.text = ''
+    //this.query = '沖縄県'
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      text: null,
+      query: '沖縄県'
+    }
   }
 
   componentDidMount () {
+    this.loadarea(this.URI)
+  }
+
+  handleChange () {
     this.loadarea(this.URI)
   }
 
@@ -54,16 +65,47 @@ class App extends React.Component {
     console.log(this.state)
     return (
       <div>
-        <h1>API1</h1>
-        <p>
-          Powered by{' '}
-          <a href='http://webservice.recruit.co.jp/'>
-            ホットペッパー Webサービス
-          </a>
-        </p>
+        <form onsubmit=''>
+          <input
+            type='text'
+            onChange={e => this.setState({ query: e.target.value })}
+            value={this.text}
+          />
+        </form>
+        <Button
+          variant='contained'
+          color='primary'
+          href='#contained-buttons'
+          onClick={this.handleChange()}
+        >
+          検索
+        </Button>
       </div>
     )
   }
+}
+
+const Sarch = props => {
+  const [text, setText] = React.useState('')
+  return (
+    <div className='main'>
+      <form onSubmit=''>
+        <input
+          type='text'
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+      </form>
+      <Button
+        variant='contained'
+        color='primary'
+        href='#contained-buttons'
+        onClick={props.query}
+      >
+        検索
+      </Button>
+    </div>
+  )
 }
 
 export default App
